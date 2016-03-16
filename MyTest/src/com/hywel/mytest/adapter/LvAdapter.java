e821ea717eb8cd3f2f34dc0130169f4bb9f80d03
Hywel <1193560789@qq.com>
2016-03-16 16:49:57 +0800

    change setting

--- MyTest/src/com/hywel/mytest/adapter/LvAdapter.java
+++ MyTest/src/com/hywel/mytest/adapter/LvAdapter.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.hywel.mytest.R;
 
 import java.util.List;
 
@@ -15,23 +18,47 @@ public class LvAdapter<T> extends BaseAdapter {
 
     private List<T> mList;
 
+    public LvAdapter(Context context, List<T> mList) {
+        this.context = context;
+        this.mList = mList;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return mList == null ? 0 : mList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return mList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            holder.picImageView = (ImageView) convertView.findViewById(R.id.iv_pic);
+            holder.titleTextView = (TextView) convertView.findViewById(R.id.tv_title);
+            holder.desTextView = (TextView) convertView.findViewById(R.id.tv_des);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
         return null;
+    }
+
+    class ViewHolder {
+        ImageView picImageView;
+
+        TextView titleTextView;
+
+        TextView desTextView;
     }
 }
