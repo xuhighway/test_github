100b1bafa201bc02491ec3db6d882f1df539e461
Hywel <1193560789@qq.com>
2016-03-16 09:37:38 +0800

    change dev tools

---
+++ MyTest/src/com/hywel/mytest/adapter/LvAdapter.java
@@ -0,0 +1,37 @@
+package com.hywel.mytest.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+/**
+ * Created by hywel on 16/3/16.
+ */
+public class LvAdapter<T> extends BaseAdapter {
+    private Context context;
+
+    private List<T> mList;
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
